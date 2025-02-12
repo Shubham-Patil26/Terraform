bec0cc7181ee1f600d60a344bf292b024eccf555
Shubham <Shubhampatil8357@gmail.com>
2025-02-12 21:20:23 +0530

    updated

--- Instance.tf
+++ Instance.tf
@@ -73,28 +73,28 @@
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]   Allow SSH from anywhere
+      cidr_blocks = ["0.0.0.0/0"]  #Allow SSH from anywhere
     }
 
     ingress {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]   Allow HTTP traffic
+      cidr_blocks = ["0.0.0.0/0"]  #Allow HTTP traffic
     }
 
     ingress {
       from_port   = 443
       to_port     = 443
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]   Allow HTTPS traffic
+      cidr_blocks = ["0.0.0.0/0"]  #Allow HTTPS traffic
     }
 
     egress {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]   Allow all outbound traffic
+      cidr_blocks = ["0.0.0.0/0"]  #Allow all outbound traffic
     }
 
     tags = {
